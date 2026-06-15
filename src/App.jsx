032449a19bec85f721a66de88eec2f90fe0f3719
Dihoccode1” <khdidenroiday@gmail.com>
2026-06-15 17:03:50 +0700

    finish code-base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Toaster position="top-right" />
+    </>
+  );
 };
 
 export default App;
